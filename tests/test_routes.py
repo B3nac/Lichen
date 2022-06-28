0357f4fd39050af2061e9b92257be9596cf0448d
B3nac <kyle.benac@gmail.com>
2022-06-27 18:37:55 -0700

    Update route tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,17 +1,21 @@
-from conftest import app_test as flask_app
 import requests
 
 
 def test_index_route_get():
-    response = requests.get("http://localhost:45380/")
+    response = requests.get("http://localhost:40682/")
     if "Unlock account" in response.text:
         assert True
     else:
         assert False
 
 
+def test_index_route_post():
+    response = requests.post("http://localhost:40682/")
+    assert response.status_code == 405
+
+
 def test_account_route_get():
-    response = requests.get("http://localhost:45380/account")
+    response = requests.get("http://localhost:40682/account")
     if "Unlock account" in response.text:
         assert True
     else:
@@ -19,10 +23,25 @@ def test_account_route_get():
 
 
 def test_account_lookup_route_get():
-    response = requests.get("http://localhost:45380/lookup")
+    response = requests.get("http://localhost:40682/lookup")
     assert response.status_code == 405
 
 
 def test_account_lookup_route_post():
-    response = requests.post("http://localhost:45380/lookup")
+    response = requests.post("http://localhost:40682/lookup")
     assert response.status_code == 400
+
+
+def test_account_create_route_get():
+    response = requests.get("http://localhost:40682/create")
+    assert response.status_code == 200
+
+
+def test_account_create_route_post():
+    response = requests.get("http://localhost:40682/create")
+    assert response.status_code == 200
+
+
+def test_send_route_get():
+    response = requests.get("http://localhost:40682/send")
+    assert response.status_code == 200
