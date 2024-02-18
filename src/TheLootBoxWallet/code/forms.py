bd3b06b7d0c756cede75511ef4842a3899fba583
B3nac <kyle.benac@gmail.com>
2024-02-17 22:55:03 -0800

    Settings template input validation progress

--- src/TheLootBoxWallet/code/forms.py
+++ src/TheLootBoxWallet/code/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms.csrf.session import SessionCSRF
 from wtforms import StringField, SubmitField, validators, BooleanField, IntegerField
 from wtforms.widgets import PasswordInput
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, URL
 from datetime import timedelta
 import string
 import secrets
@@ -118,9 +118,9 @@ class SendVerifyForm(FlaskForm):
             return session
 
 class SettingsForm(FlaskForm):
-    network = StringField('Network endpoint', validators=[InputRequired(), Length(min=0, max=42)])
-    default_address = StringField('Default Ethereum address', validators=[InputRequired(), Length(min=0, max=42)])
-    ens_mainnet_node = StringField('ENS mainnet endpoint', validators=[InputRequired(), Length(min=0, max=42)])
+    network = StringField('Network endpoint', validators=[InputRequired(), URL()])
+    default_address = StringField('Default Ethereum address', validators=[InputRequired(), Length(min=42, max=42)])
+    ens_mainnet_node = StringField('ENS mainnet endpoint', validators=[InputRequired(), URL()])
     submit = SubmitField('Save')
 
     class Meta:
